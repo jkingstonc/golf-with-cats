310b050a152df2b23209ddf9c6f3a71c41654938
James Clarke <j.kingston.c@gmail.com>
2025-02-16 18:18:30 +0000

    init

---
+++ scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+using Godot;
+
+public partial class PlayerController : Node2D
+{
+	private enum SwingState {
+		IDLE,
+		SWINGING
+	}
+
+	private SwingState swingState;
+	private Vector2 swingStartingPos;
+
+	public override void _Ready()
+	{
+		swingState = SwingState.IDLE;
+	}
+
+	public override void _Process(double delta)
+	{
+		if(swingState==SwingState.IDLE){
+			if(Input.IsActionJustPressed("swing")){
+				GD.Print("left");
+				swingState = SwingState.SWINGING;
+				swingStartingPos = GetGlobalMousePosition();
+			}
+		}else if(swingState==SwingState.SWINGING){
+			if(Input.IsActionJustReleased("swing")){
+				var diff = GetGlobalMousePosition()-swingStartingPos;
+				var dir = -diff.Normalized();
+				var power = diff.Length();
+				GD.Print($"dir = {dir} power = {power}");
+
+				GetNode<RigidBody2D>("/root/Root/Ball").ApplyImpulse(dir * power);
+
+				swingState = SwingState.IDLE;
+			}
+		}
+	}
+}
